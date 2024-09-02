506003893bfec51e7d127849ad0edba6fc88c268
ZengZ-233 <147756443+ZengZ-233@users.noreply.github.com>
2024-09-02 16:54:36 +0800

    Add files via upload

--- Leetcode/二分查找/981-基于时间的键值存储.py
+++ Leetcode/二分查找/981-基于时间的键值存储.py
@@ -1,35 +1,34 @@
 """
 https://leetcode.cn/problems/time-based-key-value-store/?envType=study-plan-v2&envId=binary-search
 """
-import collections
-from bisect import bisect
+from collections import defaultdict
+import bisect
 
 
 class TimeMap:
+
     def __init__(self):
-        self.ans = collections.defaultdict(list)
-        self.time = dict()
+        self.tm = defaultdict(list)
+        self.value = defaultdict(list)
+
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        bisect.insort(self.ans[key], timestamp)  # 升序插入timestamp
-        self.time[timestamp] = value
-        """
-        time:                    ans:
-        {1: 'bar'}               defaultdict(<class 'list'>, {'foo': [1]})
-        {1: 'bar'}               defaultdict(<class 'list'>, {'foo': [1]})
-        {1: 'bar', 4: 'bar2'}    defaultdict(<class 'list'>, {'foo': [1, 4]})
-        {1: 'bar', 4: 'bar2'}    defaultdict(<class 'list'>, {'foo': [1, 4]})
-        """
+        self.value[key].append(value)
+        self.tm[key].append(timestamp)
+
 
     def get(self, key: str, timestamp: int) -> str:
-        print(self.time)
-        if key in self.ans and self.ans[key][0] <= timestamp:
-            k = bisect.bisect(self.ans[key], timestamp)  # 二分查找timestamp的索引位置
-            return self.time[self.ans[key][k - 1]]
+        if not self.tm[key]:
+            return ""
+        i = bisect.bisect_left(self.tm[key], timestamp)
+        if 0<=i<len(self.tm[key]) and self.tm[key][i] == timestamp:
+            return self.value[key][i]
+        if 0<=i-1<len(self.tm[key]) and self.tm[key][i-1]<=timestamp:
+            return self.value[key][i-1]
         return ""
+
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
-
